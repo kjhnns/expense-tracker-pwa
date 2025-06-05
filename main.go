b3740a9a6eb0770c52b4fc0d447aeae5b87d5ad3
Johannes Klumpe <klumpe@live.de>
2025-06-05 18:14:42 +0200

    Merge master and extend group schema

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
-    "net/http"
-    "github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-    r := chi.NewRouter()
-    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Expense Tracker API"))
-    })
+	db := initDB("data.db")
+	defer db.Close()
 
-    http.ListenAndServe(":8080", r)
+	r := chi.NewRouter()
+
+	// Serve the frontend index.html when the root path is requested
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./frontend/index.html")
+	})
+
+	// Serve static frontend assets such as app.js and component files
+	fileServer := http.FileServer(http.Dir("./frontend"))
+	r.Handle("/*", http.StripPrefix("/", fileServer))
+
+	r.Post("/api/groups", createGroupHandler(db))
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
