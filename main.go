32d0a1633b27724f16fa5ebefb1a3e6afc337a9f
Johannes Klumpe <klumpe@live.de>
2025-06-05 17:55:54 +0200

    Serve frontend index at root

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-    "net/http"
-    "github.com/go-chi/chi/v5"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-    r := chi.NewRouter()
-    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Expense Tracker API"))
-    })
+	r := chi.NewRouter()
 
-    http.ListenAndServe(":8080", r)
+	// Serve the frontend index.html when the root path is requested
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./frontend/index.html")
+	})
+
+	// Serve static frontend assets such as app.js and component files
+	fileServer := http.FileServer(http.Dir("./frontend"))
+	r.Handle("/*", http.StripPrefix("/", fileServer))
+
+	http.ListenAndServe(":8080", r)
 }
